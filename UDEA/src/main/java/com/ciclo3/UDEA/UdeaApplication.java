7ab67754ed87230c072876c6369f8ddb5909e7cf
mimolmon <81603136+mimolmon@users.noreply.github.com>
2022-09-11 21:56:42 -0500

    Agregando archivos de sprint boot

---
+++ UDEA/src/main/java/com/ciclo3/UDEA/UdeaApplication.java
@@ -0,0 +1,13 @@
+package com.ciclo3.UDEA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdeaApplication.class, args);
+	}
+
+}
---
+++ UDEA/src/test/java/com/ciclo3/UDEA/UdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclo3.UDEA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
